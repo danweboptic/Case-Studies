2a49c174eeb4666e741fe0bf3fc0108f8a9edc52
danweboptic <38462706+danweboptic@users.noreply.github.com>
2025-05-13 08:52:40 +0100

    Add files via upload

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,8 @@
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
-// Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
-// stop passing in HOST, so we can remove this workaround after the next major release.
+// Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server.
 if (
   process.env.HOST &&
-  (!process.env.SHOPIFY_APP_URL ||
-    process.env.SHOPIFY_APP_URL === process.env.HOST)
+  (!process.env.SHOPIFY_APP_URL || process.env.SHOPIFY_APP_URL === process.env.HOST)
 ) {
   process.env.SHOPIFY_APP_URL = process.env.HOST;
   delete process.env.HOST;
@@ -15,6 +13,10 @@ module.exports = {
   ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
   serverModuleFormat: "cjs",
+  serverBuildTarget: "vercel",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
-  future: {},
+  future: {
+    v2_routeConvention: true,
+  },
 };
