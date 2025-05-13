d821c9acea736eebff6c7f010c453032d07742bf
danweboptic <38462706+danweboptic@users.noreply.github.com>
2025-05-13 13:30:03 +0100

    Add files via upload

--- app/routes/app.debug.jsx
+++ app/routes/app.debug.jsx
@@ -1,5 +1,5 @@
 import { json } from "@remix-run/node";
-import prisma from "~/db.server";
+import prisma from "../db.server";
 
 export const loader = async () => {
   const sessions = await prisma.session.findMany();
